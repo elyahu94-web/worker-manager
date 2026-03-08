e7f80fcfed0c5fb3914817e1688ba3c31de75770
elyahu94-web <elyahu94@gmail.com>
2026-03-08 23:45:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'worker-manager-v26';
+var CACHE = 'worker-manager-v27';
 var ASSETS = ['./', './index.html', './manifest.json', './logo-header.png'];
 
 self.addEventListener('install', function(e) {
